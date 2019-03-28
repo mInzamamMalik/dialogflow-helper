2ed4625b4b886ac2eb6a23d5a1784766e072fa7d
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2019-03-28 18:59:52 +0500

    minor

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export { entityEntryInterface, entityv1 } from './src/entityv1'
-export { agent, userEntityv2, nodejsClient, cred } from './src/userEntityv2'
+export { agent, userEntityv2, nodejsClient, init } from './src/userEntityv2'
